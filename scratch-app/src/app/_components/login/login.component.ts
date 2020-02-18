00cdc290cd66edc351dd3e8b1117064078c26560
daniel-desouza <daniel.desouza18@gmail.com>
2020-02-18 10:12:40 -0500

    add login component

--- scratch-app/src/app/_components/login/login.component.ts
+++ scratch-app/src/app/_components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -13,13 +15,30 @@ export class LoginComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
-    private router: Router) { }
+    private router: Router,
+    private http: HttpClient) { }
 
   ngOnInit() {
+    sessionStorage.setItem('token', '');
+
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
 
+  login() {
+    let url = 'http://localhost:8080/login';
+    let result = this.http.post<Observable<boolean>>(url, {
+      username: 'user',
+      password: 'password'
+    }).subscribe(isValid => {
+      if (isValid) {
+        sessionStorage.setItem('token', btoa('user:password'));
+        this.router.navigate(['']);
+      } else {
+        alert('Authentication Failed');
+      }
+    });
+  }
 }
