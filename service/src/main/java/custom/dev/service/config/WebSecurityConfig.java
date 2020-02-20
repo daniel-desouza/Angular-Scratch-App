128f461c85fcaba958d5314e43a4049795891e0a
daniel-desouza <daniel.desouza18@gmail.com>
2020-02-19 22:51:48 -0500

    continue making spring security integration with postgres db

--- service/src/main/java/custom/dev/service/config/WebSecurityConfig.java
+++ service/src/main/java/custom/dev/service/config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package custom.dev.service.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,20 +13,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 import custom.dev.service.filter.CustomFilter;
+import custom.dev.service.security.JwtAuthEntryPoint;
+import custom.dev.service.security.JwtAuthTokenFilter;
+import custom.dev.service.services.UserDetailsServiceImpl;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    // @Override
-    // protected void configure(AuthenticationManagerBuilder auth)
-    //   throws Exception {
-    //     auth
-    //       .inMemoryAuthentication()
-    //       .withUser("user")
-    //       .password("password")
-    //       .roles("USER");
-	// }
 
 	@Autowired
     UserDetailsServiceImpl userDetailsService;
---
+++ service/src/main/java/custom/dev/service/services/AuthRestAPIs.java
@@ -0,0 +1,113 @@
+package custom.dev.service.services;
+
+import java.util.HashSet;
+import java.util.Set;
+ 
+import javax.validation.Valid;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+import custom.dev.service.services.request.LoginForm;
+import custom.dev.service.services.request.RegisterForm;
+import custom.dev.service.services.response.JwtResponse;
+import custom.dev.service.model.Role;
+import custom.dev.service.model.RoleName;
+import custom.dev.service.model.User;
+import custom.dev.service.repository.RoleRepository;
+import custom.dev.service.repository.UserRepository;
+import custom.dev.service.security.JwtProvider;
+ 
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthRestAPIs {
+ 
+    @Autowired
+    AuthenticationManager authenticationManager;
+ 
+    @Autowired
+    UserRepository userRepository;
+ 
+    @Autowired
+    RoleRepository roleRepository;
+ 
+    @Autowired
+    PasswordEncoder encoder;
+ 
+    @Autowired
+    JwtProvider jwtProvider;
+ 
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginForm loginRequest) {
+ 
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+ 
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+ 
+        String jwt = jwtProvider.generateJwtToken(authentication);
+        return ResponseEntity.ok(new JwtResponse(jwt));
+    }
+ 
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@Valid @RequestBody RegisterForm signUpRequest) {
+        if(userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return new ResponseEntity<String>("Fail -> Username is already taken!",
+                    HttpStatus.BAD_REQUEST);
+        }
+ 
+        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return new ResponseEntity<String>("Fail -> Email is already in use!",
+                    HttpStatus.BAD_REQUEST);
+        }
+ 
+        // Creating user's account
+        User user = new User(signUpRequest.getName(), signUpRequest.getUsername(),
+                signUpRequest.getEmail(), encoder.encode(signUpRequest.getPassword()));
+ 
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+ 
+        strRoles.forEach(role -> {
+          switch(role) {
+          case "admin":
+            Role adminRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
+                  .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+            roles.add(adminRole);
+            
+            break;
+          case "pm":
+                Role pmRole = roleRepository.findByName(RoleName.ROLE_PM)
+                  .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+                roles.add(pmRole);
+                
+            break;
+          default:
+              Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                  .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+              roles.add(userRole);              
+          }
+        });
+        
+        user.setRoles(roles);
+        userRepository.save(user);
+ 
+        return ResponseEntity.ok().body("User registered successfully!");
+    }
+}
