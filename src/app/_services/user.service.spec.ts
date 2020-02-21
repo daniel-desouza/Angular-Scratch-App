e2701a223443a5714604a8ff4220b8181a09fc0c
Daniel de Souza <daniel.desouza18@gmail.com>
2020-02-21 14:41:16 -0500

    fix unit test for AppComponent

--- src/app/_services/user.service.spec.ts
+++ src/app/_services/user.service.spec.ts
@@ -1,12 +1,33 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../_models/user';
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: UserService;
+  let http: HttpClient;
+
+  beforeEach(() => { service = new UserService(http); });
 
   // it('#register should return json value from observable', () => {
-  //   const service: UserService = TestBed.get(UserService);
-  //   expect(service).toBeTruthy();
+    // let user: User;
+    // user = {
+    //   id: 1,
+    //   username: 'qwreqwer',
+    //   password: 'qwerqwer',
+    //   name: 'tony',
+    //   email: 'asdf@asdf.asdf',
+    //   role: 'admin',
+    //   token: ''
+    // }
+
+    // (done: DoneFn) => {
+    //   service.getObservableValue().subscribe(value => {
+    //     expect(value).toBe('observable value');
+    //     done();
+    //   });
+
+    // expect(service.register(user)).toBeTruthy();
+
   // });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,84 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HomeComponent } from './_components/home/home.component';
+import { AboutComponent } from './_components/about/about.component';
+import { DemoComponent } from './_components/demo/demo.component';
+import { AlertModule } from './_alert';
+import { MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule,
+  MatInputModule, MatProgressBarModule, MatProgressSpinnerModule, MatSidenavModule,
+  MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatTabsModule,
+  MatToolbarModule, MatTooltipModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';    
+import { RegisterComponent } from './_components/register/register.component';
+import { LoginComponent } from './_components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VelocityComponent } from './_components/demo/template-components/velocity/velocity.component';
+import { FreemarkerComponent } from './_components/demo/template-components/freemarker/freemarker.component';
+import { ThymeleafComponent } from './_components/demo/template-components/thymeleaf/thymeleaf.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        AboutComponent,
+        DemoComponent,
+        RegisterComponent,
+        LoginComponent,
+        VelocityComponent,
+        FreemarkerComponent,
+        ThymeleafComponent
       ],
+      imports: [
+        AlertModule,
+        AppRoutingModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatSidenavModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatSnackBarModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        OverlayModule
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     }).compileComponents();
   }));
 
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
 
-  // it(`should have as title 'scratch-app'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('scratch-app');
-  // });
+  it(`should have as title 'scratch-app'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('scratch-app');
+  });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('scratch-app app is running!');
-  // });
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#title-button span').textContent).toContain('Angular Sandbox');
+  });
 });
