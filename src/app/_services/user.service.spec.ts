9a86e563e90beb146048b1eb6b9524dd32847400
Daniel de Souza <daniel.desouza18@gmail.com>
2020-02-21 15:12:24 -0500

    try an HttpClient test

--- src/app/_services/user.service.spec.ts
+++ src/app/_services/user.service.spec.ts
@@ -1,33 +1,55 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { UserService } from './user.service';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../_models/user';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('UserService', () => {
   let service: UserService;
   let http: HttpClient;
 
-  beforeEach(() => { service = new UserService(http); });
+  beforeEach(() => { TestBed.configureTestingModule({
+      providers: [UserService],
+      imports: [HttpClientTestingModule]
+    });
+  });
 
-  // it('#register should return json value from observable', () => {
-    // let user: User;
-    // user = {
-    //   id: 1,
-    //   username: 'qwreqwer',
-    //   password: 'qwerqwer',
-    //   name: 'tony',
-    //   email: 'asdf@asdf.asdf',
-    //   role: 'admin',
-    //   token: ''
-    // }
+  afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    httpMock.verify();
+  }));
 
-    // (done: DoneFn) => {
-    //   service.getObservableValue().subscribe(value => {
-    //     expect(value).toBe('observable value');
-    //     done();
-    //   });
+  it('#register should return json value from observable',
+    inject([HttpTestingController, UserService],
+      (httpMock: HttpTestingController, service: UserService) => {
+    let user: User;
+    user = {
+      id: 1,
+      username: 'qwreqwer',
+      password: 'qwerqwer',
+      name: 'tony',
+      email: 'asdf@asdf.asdf',
+      role: 'admin',
+      token: ''
+    };
 
-    // expect(service.register(user)).toBeTruthy();
+    service.register(user).subscribe(data => {
+      console.log(data);
+      expect(data[0].id).toBe(1);
+    });
 
-  // });
+    const req = httpMock.expectOne('http://localhost:8080/api/auth/register');
+    console.log('req method:', req.request.method);
+    
+    expect(req.request.method).toEqual('POST');
+
+    req.flush({data: {
+      id: 1,
+      username: 'qwreqwer',
+      password: 'qwerqwer',
+      name: 'tony',
+      email: 'asdf@asdf.asdf',
+      role: 'admin',
+      token: ''
+    }});
+  }));
 });
